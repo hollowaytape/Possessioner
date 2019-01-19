b41c7e773a987bea471f0e245e94648839dcfb5c
hollowaytape <hollowaytape@retro-type.com>
2019-01-19 00:28:38 -0500

    Basic dump/reinsert; nametag ASM hack

---
+++ reinsert.py
@@ -0,0 +1,112 @@
+"""
+    Possessioner reinserter.
+    Based on the CRW reinserter base via Pssr98-II.
+"""
+
+import os
+
+from rominfo import FILE_BLOCKS, ORIGINAL_ROM_PATH, TARGET_ROM_PATH
+from romtools.disk import Disk, Gamefile, Block
+from romtools.dump import DumpExcel
+
+DUMP_XLS_PATH = 'PSSR_dump.xlsx'
+
+Dump = DumpExcel(DUMP_XLS_PATH)
+OriginalPssr = Disk(ORIGINAL_ROM_PATH, dump_excel=Dump)
+TargetPssr = Disk(TARGET_ROM_PATH)
+
+FILES_TO_REINSERT = ['POS.EXE', 'POSM.EXE']
+
+for filename in FILES_TO_REINSERT:
+    path_in_disk = "PSSR\\"
+    gamefile_path = os.path.join('original', filename)
+
+    gamefile = Gamefile(gamefile_path, disk=OriginalPssr, dest_disk=TargetPssr)
+
+    if filename == 'POS.EXE':
+        # Ascii text hack for nametags, see notes.txt
+        gamefile.edit(0xa0ba, b'\x74\x15\x30\xe4\x90\x90\x90\x90\x90\x90\x90\x90')
+        gamefile.edit(0xa0cd, b'\x90\x90')
+
+
+    for block in FILE_BLOCKS[filename]:
+        block = Block(gamefile, block)
+        print(block)
+        previous_text_offset = block.start
+        diff = 0
+        for t in Dump.get_translations(block, include_blank=True):
+            #print(t.english)
+            loc_in_block = t.location - block.start + diff
+
+            this_diff = len(t.en_bytestring) - len(t.jp_bytestring)
+            if this_diff > 0:
+                print(t.en_bytestring, t.jp_bytestring)
+                raise Exception
+            while this_diff < 0 and t.en_bytestring != b'':
+                t.en_bytestring += b' '
+                this_diff = len(t.en_bytestring) - len(t.jp_bytestring)
+
+            if t.en_bytestring != b'':
+                print(t.en_bytestring)
+
+
+            """
+            if filename.endswith('.DAT'):
+                print(t.en_bytestring)
+                print(t.jp_bytestring)
+                print("Diff is ", this_diff)
+                # Need to pad with 00's
+                while this_diff < 0:
+                    t.en_bytestring += b'\x00'
+                    this_diff += 1
+                while this_diff > 0:
+                    t.jp_bytestring += b'\x00'
+                    this_diff -= 1
+
+                assert len(t.en_bytestring) - len(t.jp_bytestring) == 0
+            """
+
+            if t.english == b'' or t.english == t.japanese:
+                #print(hex(t.location), t.english, "Blank string")
+                this_diff = 0
+                #print("Diff is", diff)
+
+                # No pointers!
+                #gamefile.edit_pointers_in_range((previous_text_offset, t.location), diff)
+                previous_text_offset = t.location
+                continue
+
+            try:
+                i = block.blockstring.index(t.jp_bytestring)
+            except ValueError:
+                print(t, "wasn't found in the string. Skipping for now")
+                continue
+            j = block.blockstring.count(t.jp_bytestring)
+
+            index = 0
+            while index < len(block.blockstring):
+                index = block.blockstring.find(t.jp_bytestring, index)
+                if index == -1:
+                    break
+                index += len(t.jp_bytestring) # +2 because len('ll') == 2
+
+            assert loc_in_block == i, (t, hex(loc_in_block), hex(i))
+
+            block.blockstring = block.blockstring.replace(t.jp_bytestring, t.en_bytestring, 1)
+
+            # No pointers!
+            #gamefile.edit_pointers_in_range((previous_text_offset, t.location), diff)
+            previous_text_offset = t.location
+
+            diff += this_diff
+            #print("Diff is", diff)
+
+        block_diff = len(block.blockstring) - len(block.original_blockstring)
+        if block_diff < 0:
+            block.blockstring += (-1)*block_diff*b'\x00'
+        block_diff = len(block.blockstring) - len(block.original_blockstring)
+        assert block_diff == 0, block_diff
+
+        block.incorporate()
+
+    gamefile.write(path_in_disk=path_in_disk)
---
+++ rominfo.py
@@ -0,0 +1,66 @@
+"""
+    Rom description of Possessioner.
+"""
+
+import os
+
+ORIGINAL_ROM_DIR = 'original'
+TARGET_ROM_DIR = 'patched'
+
+ORIGINAL_ROM_PATH = os.path.join(ORIGINAL_ROM_DIR, 'Possessioner.hdi')
+TARGET_ROM_PATH = os.path.join(TARGET_ROM_DIR, 'Possessioner.hdi')
+DUMP_XLS_PATH = 'PSSR_dump.xlsx'
+
+FILES = ['POSM.EXE', 'POS.EXE', 'POS1.MSD', 'P_7.MSD', 'P_71.MSD', 
+         'P_BILL.MLL', 'P_BILL.MSD', 'P_BOX.MSD', 'P_BYO.MSD',
+         'P_CITY.MSD', 'P_ENT.MSD', 'P_ENT2.MSD', 'P_GE.MSD', 
+         'P_GYOTEI.MSD', 'P_GYOTEI.MSG', 'P_HI.MSD', 'P_HON1.MSD',
+         'P_HOU.MSD', 'P_JUNK.MSD', 'P_KYU.MSD', 'P_ROU1.MSD',
+         'P_SE.MSD', 'P_SIRYO.MSD', 'P_SUTE.MSD', 'P_SW1.MSD',
+         'P_SYO.MSD', 'RASU1.MSD', 'RASU2.MSD', 'STAFF.TXT',
+         'TINA.MSD', 'YUMI.MSD', 'ARISA.MSD', 'AYAKA.MSD',
+         'ERIS.MSD', 'HONHOA.MSD', 'MAI.MSD', 'MERYL.MSD', 
+         'MINS.MSD', 'MISHA.MSD', 'NEDRA1.MSD', 'NEDRA2.MSD',
+         'DOCTOR.MSD', 'PLYM.MSD', 
+         'POSE.EXE', 'END.MSD', ]
+
+# .M files have song titles/descriptions in them, probably just internal
+# "Arisa, songs of being fucked by the machine"
+
+
+FILE_BLOCKS = {
+    #"""
+    #    Text for the editor itself
+    #"""
+    "POS.EXE": [
+        (0x7fcf, 0x800b),
+        (0x8b03, 0x9329),
+        (0xa122, 0xa397),  # table thing
+        (0xab98, 0xac3e),
+        (0xcf61, 0xd12a),
+        (0xdb2d, 0xdb3f), # Arisa
+        (0xdb63, 0xdb74), # Hong Ho
+        (0xdb99, 0xdbaa), # Melissa
+        (0xdbcf, 0xdbe0), # Sedora
+        # Some more annoying ones to map between 0xdc13 and 0sdd93
+        (0xe00b, 0xe1fb),
+        # and a ton more between ~0x17000 and 0x28000
+    ],
+
+    "POSE.EXE": [
+        (0x9628, 0x9648), # errors
+        (0x98fe, 0x9cfc), # ending text?
+    ],
+
+    'POSM.EXE': [
+        (0xb238, 0xb450),  # sound test songs
+        (0xb88c, 0xbf18),  # intro text
+    ]
+}
+
+# default to dumping the whole file
+for f in FILES:
+    if f not in FILE_BLOCKS:
+        FILE_BLOCKS[f] = [(0, 0xfffff)]
+
+assert len(FILE_BLOCKS) == len(FILES), "%s %s" % (len(FILE_BLOCKS), len(FILES))
